c936eb3e40bad01c0eaf9ca0405e36dd77b26622
SIDHANT-SIN <sidhantsingh54321ss@gmail.com>
2025-07-01 13:57:50 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,36 @@
 const express = require("express");
-// const http = require("http");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-//const Account = require("./models/accounts");
-//
-app.use(cors());
+
 app.use(express.json());
 require("dotenv").config();
 require("./contestUpdation/cron");
 
+const allowedOrigins = ["https://algopulse-site.netlify.app"];
 
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+  })
+);
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error("MongoDB Error:", err));
-
 
 const accountRoutes = require("./routes");
 app.use("/api/accounts", accountRoutes);
